1c14f9efea9657f19f8abc2e8cb03fd2a9ea92f6
Thiemo <not_a@real_email.com>
2020-03-16 17:30:58 +0100

    Square-shaped buttons

--- src/main/com.calabi.pixelator/com/calabi/pixelator/control/basic/ToggleImageButton.java
+++ src/main/com.calabi.pixelator/com/calabi/pixelator/control/basic/ToggleImageButton.java
@@ -2,6 +2,7 @@ package com.calabi.pixelator.control.basic;
 
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import com.calabi.pixelator.res.Images;
@@ -18,13 +19,14 @@ public class ToggleImageButton extends ToggleButton {
     }
 
     public ToggleImageButton(Images image, Images selected) {
-        ImageView imageView = new ImageView(image.getImage());
-        ImageView selectedView = new ImageView(selected.getImage());
+        Image image1 = image.getImage();
+        Image image2 = selected.getImage();
+        ImageView imageView = new ImageView(image1);
         selectedProperty().addListener((ov, o, n) -> {
             if (n) {
-                setGraphic(selectedView);
+                imageView.setImage(image2);
             } else {
-                setGraphic(imageView);
+                imageView.setImage(image1);
             }
         });
 
