47141ffddfd1216df9b6d96998d3de54fa420224
Thiemo Fischer <fischer.thiemo@web.de>
2021-11-01 14:27:10 +0100

    Fixed and documented build process

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 The lovingly hand-crafted graphics editor for pixel art.
 
-Pixelator is designed to be intuitive and easy to use while providing powerful tools that will make your life easier.
+Pixelator is intuitive and easy to use while providing powerful tools that will make your daily work faster and more fun.
 
 Absolutely free and open-source.
+
+
+## Build
+
+1. Build with IntelliJ
+2. mvn package
+3. JAR with dependencies can be found at ./target/pixelator-X.X.X.jar
+
+
+## Installation
+
+1. Download current pixelator.jar
+2. Run pixelator.jar
+
+Windows:
+
+Double-click pixelator.jar
+
+Linux:
+
+> java -jar pixelator.jar
--- src/main/com.calabi.pixelator/com/calabi/pixelator/res/BuildInfo.java
+++ src/main/com.calabi.pixelator/com/calabi/pixelator/res/BuildInfo.java
@@ -1,27 +1,11 @@
 package com.calabi.pixelator.res;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import com.calabi.pixelator.start.ExceptionHandler;
-
 public final class BuildInfo {
 
     public static final String VERSION;
 
-    private static final Properties properties = new Properties();
-
     static {
-        try (InputStream inputStream = BuildInfo.class.getResourceAsStream("/build-info.properties")) {
-            if (inputStream != null) {
-                properties.load(inputStream);
-            }
-        } catch (final IOException e) {
-            ExceptionHandler.handle(e);
-        }
-
-        VERSION = properties.getProperty("pom.version");
+        VERSION = BuildInfo.class.getPackage().getImplementationVersion();
     }
 
     private BuildInfo() {
